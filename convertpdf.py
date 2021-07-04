b1f301ace246751e3098a9c739afbcf28220c151
Shankar Dutt <shankar.dutt@anu.edu.au>
2021-07-04 11:43:24 +1000

    updated options to convert in two resolutions

--- convertpdf.py
+++ convertpdf.py
@@ -11,29 +11,47 @@ st.set_page_config(page_title="Convert PDF to JPEG", initial_sidebar_state="expa
 st.sidebar.title("Convert PDF to JPEG")
 st.sidebar.subheader("Author: Shankar Dutt (shankar.dutt@anu.edu.au) and Vanessa Moss (vanessa.moss@csiro.au)")
 
-uploaded_file = st.file_uploader("Choose a PDF file", type=['pdf'])
+option = st.sidebar.selectbox("Menu",('4096px','1024px'))
+if option =='4096px':
+    uploaded_file = st.file_uploader("Choose a PDF file", type=['pdf'])
 
-def get_image_download_link(img,filename,text):
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode()
-    href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
-    return href
+    def get_image_download_link(img,filename,text):
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+        return href
 
-if uploaded_file is not None and uploaded_file.type == "application/pdf":
-    try:
-        images = convert_from_bytes(uploaded_file.read(),size=(4096,None))
-        images_small = convert_from_bytes(uploaded_file.read(),size=(1024,None))
-        for index, page in enumerate(images):
-            st.image(page, use_column_width=True)
-            st.image(page, use_column_width=True)
-            name = os.path.splitext(uploaded_file.name)[0]
-            st.markdown(get_image_download_link(page, name+'_page_'+f'{index+1}'+'.jpg', 'Download ' + name +'_page_'+f'{index+1}'+ '.jpg'), unsafe_allow_html=True)
-        for index, page in enumerate(images_small):
-            st.image(page, use_column_width=True)
-            st.image(page, use_column_width=True)
-            name = os.path.splitext(uploaded_file.name)[0]
-            st.markdown(get_image_download_link(page, name+'_page_'+f'{index+1}'+'_thumb'+'.jpg', 'Download ' + name +'_page_'+f'{index+1}'+'_thumb'+ '.jpg'), unsafe_allow_html=True)
-    except:
-        st.write("Choose different file. Input file not correct")
+    if uploaded_file is not None and uploaded_file.type == "application/pdf":
+        try:
+            images = convert_from_bytes(uploaded_file.read(),size=(4096,None))
+            for index, page in enumerate(images):
+                st.image(page, use_column_width=True)
+                name = os.path.splitext(uploaded_file.name)[0]
+                st.markdown(get_image_download_link(page, name+'_page_'+f'{index+1}'+'_4096px'+'.jpg', 'Download ' + name +'_page_'+f'{index+1}'+'_4096px'+ '.jpg'), unsafe_allow_html=True)
+        except:
+            st.write("Choose different file. Input file not correct")
 
+if option == '1024px':
+    uploaded_file = st.file_uploader("Choose a PDF file", type=['pdf'])
+
+
+    def get_image_download_link(img, filename, text):
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        href = f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+        return href
+
+
+    if uploaded_file is not None and uploaded_file.type == "application/pdf":
+        try:
+            images = convert_from_bytes(uploaded_file.read(), size=(1024, None))
+            for index, page in enumerate(images):
+                st.image(page, use_column_width=True)
+                name = os.path.splitext(uploaded_file.name)[0]
+                st.markdown(get_image_download_link(page, name + '_page_' + f'{index + 1}' + '_1024px' + '.jpg',
+                                                    'Download ' + name + '_page_' + f'{index + 1}' + '_1024px' + '.jpg'),
+                            unsafe_allow_html=True)
+        except:
+            st.write("Choose different file. Input file not correct")
